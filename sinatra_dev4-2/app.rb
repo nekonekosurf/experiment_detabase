e36bef6500ceff9546a07b229443d16833efb81a
nekonekosurf <nekonekosurf88@gmail.com>
2019-10-31 16:59:16 +0900

    応用課題4ー2完了

--- sinatra_dev4-2/app.rb
+++ sinatra_dev4-2/app.rb
@@ -3,7 +3,6 @@ require 'active_record'
 require 'date'
 enable :sessions
 
-
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'sandbox.db'
@@ -11,40 +10,41 @@ ActiveRecord::Base.establish_connection(
 
 class Account < ActiveRecord::Base
 end
-
 class BulletinBoard < ActiveRecord::Base
 end
 
-
+flag=0
 
 get '/' do
+  # session[:ongoing]='login page'
   @title = 'login page'
-  @user = Account.where(:user_id=>"neko")
+  if flag==0
+    @comment='ログイン画面'
+  else
+    @comment='ログインできませんでした'
+  end
   erb :index
-
 end
-
-
 
 post '/board' do
   if !Account.where(:user_id=> params[:user_id], :password=>params[:password]).empty?
     @title = 'View user'
     @user_id=params[:user_id]
     @board=BulletinBoard.all.order("id DESC")
-    global_user_id = params[:user_id]
+    session[:user_id] = params[:user_id]
     erb :login
   else
+    # session[:failed]='login failed'
+    # @comment=session[:failed]
+    flag =1
     redirect '/'
   end
 end
-#user_iわたせない
-#
 
 nowTime = DateTime.now
 post '/update' do
-  BulletinBoard.create(:user_id=>params[:user_id],:message=>params[:message],:taimu=>nowTime)#.order("id DESC").first
+  BulletinBoard.create(:user_id=>session[:user_id],:message=>params[:message],:taimu=>nowTime)
   @board=BulletinBoard.all.order("id DESC")
-
-  erb :logins
-
+  @user_id=session[:user_id]
+  erb :login
 end
